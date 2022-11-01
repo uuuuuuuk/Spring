4765041b089e5cd6c79808c7c635a67b079bb401
uuuuuuuk <s22034@gsm.hs.kr>
2022-11-01 20:32:50 +0900

    Initial Commit

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,26 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+}
